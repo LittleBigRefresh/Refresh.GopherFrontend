d109f6072eb9b7788f6fe9eb658e5cc60f66a3cb
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-10-26 00:39:25 -0700

    Freshen up user profiles on Gemini

--- Refresh.GopherFrontend/Endpoints/UserEndpoints.cs
+++ Refresh.GopherFrontend/Endpoints/UserEndpoints.cs
@@ -8,6 +8,7 @@ using Bunkum.Protocols.Gopher.Responses;
 using Bunkum.Protocols.Gopher.Responses.Items;
 using Refresh.GopherFrontend.Api;
 using Refresh.GopherFrontend.Api.Types;
+using Refresh.GopherFrontend.Extensions;
 
 namespace Refresh.GopherFrontend.Endpoints;
 
@@ -22,14 +23,12 @@ public class UserEndpoints : EndpointGroup
     {
         RefreshUser user = apiService.GetUser(username);
         
-        List<GophermapItem> map = new()
-        {
-            new GophermapMessage($"{user.Username}'s Profile"),
-            new GophermapMessage(""),
-            new GophermapMessage(user.Description),
-            new GophermapMessage($"Joined on {user.JoinDate}"),
-        };
-        
+        List<GophermapItem> map = new();
+        map.AddHeading(context, $"{user.Username}'s Profile", 1);
+        map.Add(new GophermapMessage(""));
+        map.AddHeading(context, user.Description, 2);
+        map.AddHeading(context, $"Joined on {user.JoinDate}", 3);
+
         if (user.IconHash != "0" && user.IconHash[0] != 'g')
         {
             map.Add(new GophermapLink(GophermapItemType.Image, "View User's Avatar", config, $"/user/{username}/avatar.png"));
